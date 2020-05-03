819df20ed75d3ce7064d3200de87a277f00c150d
wirya <abrarwiryawan@gmail.com>
2020-05-03 14:35:55 +0700

    Initial commit

---
+++ app/src/main/java/com/wiryatech/mynotesapp/CustomOnItemClickListener.kt
@@ -0,0 +1,15 @@
+package com.wiryatech.mynotesapp
+
+import android.view.View
+
+class CustomOnItemClickListener(private val position: Int, private val onItemClickCallback: OnItemClickCallback)
+    : View.OnClickListener {
+
+    override fun onClick(v: View) {
+        onItemClickCallback.onItemClicked(v, position)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(view: View, position: Int)
+    }
+}
---
+++ app/src/main/java/com/wiryatech/mynotesapp/helper/MappingHelper.kt
@@ -0,0 +1,23 @@
+package com.wiryatech.mynotesapp.helper
+
+import android.database.Cursor
+import com.wiryatech.mynotesapp.db.DatabaseContract
+import com.wiryatech.mynotesapp.entity.Note
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+
+}
